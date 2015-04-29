44c6a702ef48ac022543e247946479e2ec910cb9
Regan1397 <Regan1397@hotmail.com>
2015-04-30 11:51:39 +1200

    aaa

--- js/jquery_page_02.js
+++ js/jquery_page_02.js
@@ -1,92 +1,116 @@
 //work on shaking of rose
 
 $(document).ready(function() {
-var backText = 0.3;
-var textFade = 2000;
-var beeScene = 1;
-    //create a new instance of shake.js.
-    var myShakeEvent = new Shake({
-        threshold: 5
+    var backText = 0.3;
+    var textFade = 2000;
+    var beeScene = 1;
+        //create a new instance of shake.js.
+        var myShakeEvent = new Shake({
+            threshold: 5
 
-    });
+        });
 
-      // start listening to device motion
-    myShakeEvent.start();
+          // start listening to device motion
+        myShakeEvent.start();
 
-    // register a shake event
-    window.addEventListener('shake', shakeEventDidOccur, false);
-
-
-
- 
-  
-    $('body').click (function (){
-if (beeScene == 1) {
-       disturbBee();
-   }
-    });
+        // register a shake event
+        window.addEventListener('shake', shakeEventDidOccur, false);
 
 
 
-var t1;
-var t2;
-var dir1 = false;
-var dir2 = false;
-var roseTimer = 1;
+     
+      
+        $('body').click (function (){
+    if (beeScene == 1) {
+           disturbBee();
+       }
+        });
 
-function animRose () {
+
+    $(function () {
+    $('#beeBee01').bind("tap", tapHandler);
+
+        function tapHandler (event) 
+        {
+            if (beeScene == 2) {
+                stopAnimBee();
+                
+                      $('#beeBee01').animate({top: '+=5px'},100).delay(100).animate({top: '247px', left: '727px'},1500);
+                      $('#beeKing01').delay(100).attr('src', 'imgs/Bee_King_02.png');
+                      $('#beeBee01').delay(100).attr('src', 'imgs/Bee_Bee.png');
+
+                      $('#beeBee01').delay(3000, function () {
+                        $('.beeText_01 > li:nth-child(3)').fadeTo(textFade, 1);
+                        dotMove();
+                        $('.beeText_01 > li:nth-child(2)').fadeTo(textFade, backText).delay(1500);
+                        });
 
 
- t1 =setInterval(function(){
-        roseTimer +=1;
-    if (roseTimer ==10) {
-                clearInterval(t1);
             }
-    (dir1 == false) ? document.getElementById('beeRose01').style.webkitTransform = "rotate(1.5deg)" : document.getElementById('beeRose01').style.webkitTransform = "rotate(-1.5deg)";
-    dir1 = !dir1;
-}, 100);
-}
-
-function animBee () {
+        }    
+    });
 
 
- t2 =setInterval(function(){
+    var t1;
+    var t2;
+    var dir1 = false;
+    var dir2 = false;
+    var roseTimer = 1;
 
-    (dir2 == false) ? document.getElementById('beeBee01').style.webkitTransform = "rotate(21.5deg)" : document.getElementById('beeBee01').style.webkitTransform = "rotate(18.5deg)";
-    dir2 = !dir2;
-}, 1000);
-}
-
- function stopAnimBee () {
-    clearInterval(t2);
-}
+    function animRose () {
 
 
-       
-    function disturbBee () {
-        dotMove();
-         beeScene = 2;
-        $('.beeText_01 > li:nth-child(2)').fadeTo(textFade, 1);
-        $('.beeText_01 > li:nth-child(1)').fadeTo(textFade, backText,function () {
-                 AnimateRotate(20);            
-        });
-        animRose();
-        $('#beeBee01').delay(textFade).animate({top: '205px'},2000).delay(200).animate({top: '476px', left: '756px'},1500,function (){
-            animBee();
+     t1 =setInterval(function(){
+            roseTimer +=1;
+        if (roseTimer ==10) {
+                    clearInterval(t1);
+                }
+        (dir1 == false) ? document.getElementById('beeRose01').style.webkitTransform = "rotate(1.5deg)" : document.getElementById('beeRose01').style.webkitTransform = "rotate(-1.5deg)";
+        dir1 = !dir1;
+    }, 100);
+    }
 
-        });
-         
+    function animBee () {
+
+
+     t2 =setInterval(function(){
+
+        (dir2 == false) ? document.getElementById('beeBee01').style.webkitTransform = "rotate(21.5deg)" : document.getElementById('beeBee01').style.webkitTransform = "rotate(18.5deg)";
+        dir2 = !dir2;
+    }, 1000);
+    }
+
+     function stopAnimBee () {
+        clearInterval(t2);
     }
 
 
-
-    //shake event callback
-    function shakeEventDidOccur () {
-                if (beeScene ==1) {
-                var roseTimer = 1;
-                disturbBee();
-            }
+           
+        function disturbBee () {
+            dotMove();
+            beeScene = 0;
+             
+            $('.beeText_01 > li:nth-child(2)').fadeTo(textFade, 1);
+            $('.beeText_01 > li:nth-child(1)').fadeTo(textFade, backText,function () {
+                     AnimateRotate(60);            
+            });
+            animRose();
+            $('#beeBee01').delay(textFade).animate({top: '205px'},2000).animate({top: '476px', left: '756px'},1000, function (){
+                      
+                  animBee();
+                beeScene = 2;
+            });          
         }
+
+
+
+        //shake event callback
+        function shakeEventDidOccur () {
+                    if (beeScene ==1) {
+                    var roseTimer = 1;
+                    disturbBee();
+                }
+            }
 
 });
 
