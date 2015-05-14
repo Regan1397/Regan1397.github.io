9de2b70ce8b26f39fecac145e4b04eed5a025c68
Regan1397 <Regan1397@hotmail.com>
2015-05-14 23:23:20 +1200

    fasa

---
+++ js/page-script.js
@@ -0,0 +1,12 @@
+$(document).ready (function() {
+
+			$(function () {
+		 $('#img-01').bind("tap", tapHandler);
+
+		function tapHandler (event) 
+		  {
+		  	$('#img-01').fadeTo(1);
+		   }
+		});
+
+});
