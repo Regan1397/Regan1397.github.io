74f9d419f0e0be82048404141ad9402528e471a1
Regan1397 <Regan1397@hotmail.com>
2015-05-15 00:20:44 +1200

    sfafsaa

--- js/page-script.js
+++ js/page-script.js
@@ -1,11 +1,14 @@
 $(document).ready (function() {
-
+	console.log("linked");
 			$(function () {
+
 		 $('#img-01').bind("tap", tapHandler);
 
 		function tapHandler (event) 
 		  {
-		  	$('#img-01').fadeTo(1);
+		  	$('#img-01 > div').fadeTo(1000,1);
+		  	$('#img-01 > img').fadeTo(1000,0.7);
+		  	console.log("worked");
 		   }
 		});
 
